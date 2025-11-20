ff23a4d9ec64739a93050795111b7c461779147c
Thomas Han <thomas.han@live.com>
2025-11-20 17:32:18 +1100

    refactor: expand qdrant integration tests

--- app/integration-vectordb/src/vectordb/vectordb_tool.py
+++ app/integration-vectordb/src/vectordb/vectordb_tool.py
@@ -8,3 +8,6 @@ class VectorDBTool(AbstractTool):
 
     def upsert_vectors(self, collection_name: str, vectors: list[list[float]], payloads: list[dict], ids: list[str]) -> list[str]:
         pass
+
+    def delete_vectors(self, collection_name: str, ids: list[str]) -> list[str]:
+        pass
